158ccb25f8b78f85a410c1792d8aa1566779f655
revivia <vivia.vijayakumar@gmail.com>
2025-09-28 11:01:42 -0700

    added fading to black transition between screens

--- Assets/Scenes/Areas/transition.gd
+++ Assets/Scenes/Areas/transition.gd
@@ -5,4 +5,7 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"): 
+		FadeScreen.transition()
+		await FadeScreen.transition_finished
+		# might have to comment out the last 2 lines please work !!
 		get_tree().change_scene_to_file("res://casinoScene/slot_machine_ui.tscn")
