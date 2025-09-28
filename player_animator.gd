82cbde2e4b451e5b7e8cbe625bd5a28af5c110fa
jamespotato <78766754+ggkchen@users.noreply.github.com>
2025-09-28 09:51:15 -0700

    added running
    jump animation, fall animatioin, and implimented running

--- player_animator.gd
+++ player_animator.gd
@@ -8,7 +8,14 @@ func _process(_delta):
 		sprite.flip_h = false
 	elif playercontroller.direction == -1:
 		sprite.flip_h = true
-	if abs(playercontroller.velocity.x) > 0.0:
+	if Input.is_action_pressed("space"):
+		animationplayer.play("jump")
+	if abs(playercontroller.velocity.x) > 0.0 and Input.is_action_pressed("shift"):
+		animationplayer.play("run")
+	elif abs(playercontroller.velocity.x) > 0.0:
 		animationplayer.play("walk")
+	elif playercontroller.velocity.y>0:
+		animationplayer.play("falling")
 	else:
 		animationplayer.play("idle")
+	
--- player_controller.gd
+++ player_controller.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 class_name PlayerController
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -900
+const JUMP_VELOCITY = -1100
 const GRAVITY = 1800
 var direction := 0
 var start_pos = Vector2()
@@ -36,7 +36,14 @@ func _physics_process(delta: float) -> void:
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 	if direction!=0:
-		velocity.x = direction * SPEED
+		if Input.is_action_pressed("shift") and is_on_floor():
+			velocity.x = direction * SPEED * 2
+		elif Input.is_action_pressed("shift"):
+			velocity.x = direction * SPEED *1.5
+		elif is_on_floor():
+			velocity.x = direction * SPEED *0.75
+		else:
+			velocity.x = direction * SPEED *1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
