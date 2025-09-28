9f145e1004f876f599e96f24e94ccc3e4669566d
revivia <vivia.vijayakumar@gmail.com>
2025-09-28 10:11:38 -0700

    button connecting platformer and gambling scene

--- Assets/Scenes/Areas/button.gd
+++ Assets/Scenes/Areas/button.gd
@@ -1,13 +1,8 @@
 extends Button
 
 # Called when the node enters the scene tree
-func _ready():
-	# Connect the pressed signal to the local function
-	self.pressed.connect(_on_pressed)
+func _ready() -> void:
+	pressed.connect(_on_button_pressed)
 
-# Function called when the button is pressed
-func _on_pressed():
-	# Load the slot machine scene
-	var new_scene = load("res://casinoScene/slot_machine_ui.tscn")
-	# Switch to the new scene
-	get_tree().change_scene_to(new_scene)
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://casinoScene/slot_machine_ui.tscn")
